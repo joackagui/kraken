ef57c0eb83d66dbb85eb07167c49ea19d71d4575
joackagui <joaquinaguilerasalinas1@gmail.com>
2026-04-13 18:23:54 -0400

    feat: update database configuration to use environment variable for DATABASE_URL

--- kraken-backend/prisma.config.ts
+++ kraken-backend/prisma.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'prisma/config';
-//TODO: cambiar la URL de la BD para que use la variable de entorno DATABASE_URL
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
   },
   datasource: {
-    url: "postgresql://postgres:postgres@localhost:5432/uni_jira?schema=public",
+    url: process.env.DATABASE_URL,
   },
 });
